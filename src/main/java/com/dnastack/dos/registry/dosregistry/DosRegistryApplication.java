e5190ae07ddf05fc0a950d08d1526c8081d7a9c0
Marc Huang <marc.huang@scotiabank.com>
2018-02-16 00:06:55 -0500

    add in the swagger api definition file

---
+++ src/main/java/com/dnastack/dos/registry/dosregistry/DosRegistryApplication.java
@@ -0,0 +1,12 @@
+package com.dnastack.dos.registry.dosregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DosRegistryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dnastack/dos/registry/dosregistry/DosRegistryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dnastack.dos.registry.dosregistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DosRegistryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
