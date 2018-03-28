3f60c9751c3b5b41bdc94bc38deeb4966d0beaf8
Marc Huang <marc.huang@scotiabank.com>
2018-03-28 01:16:48 -0400

    add a placeholder class for elastic search implementation

---
+++ src/main/java/com/dnastack/dos/registry/downstream/elasticsearch/ElasticSearchDataClient.java
@@ -0,0 +1,17 @@
+package com.dnastack.dos.registry.downstream.elasticsearch;
+
+import com.dnastack.dos.registry.downstream.IDataClient;
+import com.dnastack.dos.registry.downstream.dto.ListDataObjectsResponseDto;
+
+/**
+ * Placeholder for ElasticSearch implementation of {@link  IDataClient}
+ *
+ * @Author: marchuang <br/>
+ * @since: 1.0.0 <br/>
+ */
+public class ElasticSearchDataClient implements IDataClient {
+    @Override
+    public ListDataObjectsResponseDto getDataObjects() {
+        return null;
+    }
+}
