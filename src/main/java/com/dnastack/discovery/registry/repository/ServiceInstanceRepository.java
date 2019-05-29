65af0ee51aca366756712af655256fabb36f506c
Milan Panik <milan.panik1@gmail.com>
2019-05-29 11:42:21 +0200

    Update dependencies

--- src/main/java/com/dnastack/discovery/registry/repository/ServiceInstanceRepository.java
+++ src/main/java/com/dnastack/discovery/registry/repository/ServiceInstanceRepository.java
@@ -2,10 +2,9 @@ package com.dnastack.discovery.registry.repository;
 
 import com.dnastack.discovery.registry.domain.ServiceInstance;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ServiceInstanceRepository extends JpaRepository<ServiceInstance, String>, QuerydslPredicateExecutor<ServiceInstance> {
+public interface ServiceInstanceRepository extends JpaRepository<ServiceInstance, String> {
 
 }
