fe72e00480fdfd3e636038e3ece967e2553907e7
Jonathan Fuerth <jonathan@dnastack.com>
2019-11-27 17:32:33 -0500

    Parameterize service-info assertions

--- e2e-tests/src/test/java/com/dnastack/discovery/registry/ServiceInfoE2ET.java
+++ e2e-tests/src/test/java/com/dnastack/discovery/registry/ServiceInfoE2ET.java
@@ -21,15 +21,15 @@ public class ServiceInfoE2ET extends BaseE2ET {
                 .log().ifValidationFails()
                 .statusCode(200)
                 .body("id", equalTo("org.ga4gh.service-registry"))
-                .body("name", equalTo("GA4GH Service Registry Reference Implementation"))
+                .body("name", equalTo(optionalEnv("E2E_SERVICE_INFO_EXPECTED_NAME", "GA4GH Service Registry Reference Implementation")))
                 .body("type", equalTo("org.ga4gh.service-registry:1.0.0"))
-                .body("description", equalTo("Reference implementation of GA4GH Service Registry"))
-                .body("organization.name", equalTo("GA4GH"))
-                .body("organization.url", equalTo("https://example.com"))
-                .body("contactUrl", equalTo("mailto:support@example.com"))
-                .body("documentationUrl", equalTo("https://github.com/ga4gh-discovery/ga4gh-service-registry"))
-                .body("environment", equalTo("dev"))
-                .body("version", equalTo("1.0.0"));
+                .body("description", equalTo(optionalEnv("E2E_SERVICE_INFO_EXPECTED_DESCRIPTION", "Reference implementation of GA4GH Service Registry")))
+                .body("organization.name", equalTo(optionalEnv("E2E_SERVICE_INFO_EXPECTED_ORGANIZATION_NAME", "GA4GH")))
+                .body("organization.url", equalTo(optionalEnv("E2E_SERVICE_INFO_EXPECTED_ORGANIZATION_URL", "https://example.com")))
+                .body("contactUrl", equalTo(optionalEnv("E2E_SERVICE_INFO_EXPECTED_CONTACT_URL", "mailto:support@example.com")))
+                .body("documentationUrl", equalTo(optionalEnv("E2E_SERVICE_INFO_EXPECTED_DOCUMENTATION_URL", "https://github.com/ga4gh-discovery/ga4gh-service-registry")))
+                .body("environment", equalTo(optionalEnv("E2E_SERVICE_INFO_EXPECTED_ENVIRONMENT", "dev")))
+                .body("version", equalTo(optionalEnv("E2E_SERVICE_INFO_EXPECTED_VERSION", "1.0.0")));
         // @formatter:on
 
     }
